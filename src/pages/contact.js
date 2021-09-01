e7fc5235b31ed1f7a873e15ffa14b74d038d9cc6
Roman <roman@overhaulmedia.com>
2021-09-01 10:38:10 -0600

    Finished Gatsby Tutorial Part 3

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const ContactPage = () => {
   return (
    <Layout pageTitle="Contact Us">
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <StaticImage
+        alt="Corporate flower needs"
+        src="../images/corporate-services-1.jpg"
+      />
     </Layout>
   )
 }
