b46331fa6c8bbd4adce52de90a8c45c9bcf78770
Roman <roman@overhaulmedia.com>
2021-09-08 14:32:26 -0600

    adding more boilerplate styles

--- src/pages/styles.js
+++ src/pages/styles.js
@@ -4,8 +4,8 @@ import { StaticImage } from 'gatsby-plugin-image'
 
 const StylesPage = () => {
     return (
-        <Layout pageTitle="Styles" class="wrapper">
-          <div class="row">
+        <Layout pageTitle="Styles (h1)" class="wrapper">
+          <section class="row">
             <StaticImage
               class="col-xs-12 col-md-6"
               alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
@@ -14,19 +14,18 @@ const StylesPage = () => {
             <StaticImage
               class="col-xs-12 col-md-6" 
               alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-              src="../images/corporate-services-3.jpg"
+              src="../images/hero-corporate.jpg"
             />
-          </div>
-          <h2>Grid Layouts</h2>
+          </section>
           <div class="row">
             <div class="col-xs-12 col-md-3">
               <div class="box-row">
-                Some Content
+                &nbsp;
               </div>
             </div>
             <div class="col-xs-12 col-md-9">
               <div class="box-row">
-                Some Content
+                 &nbsp;
               </div>
             </div>
           </div>
@@ -34,45 +33,65 @@ const StylesPage = () => {
           <div class="row">
             <div class="col-xs">
               <div class="box-row">
-                Some Content
+                 &nbsp;
               </div>
             </div>
             <div class="col-xs">
               <div class="box-row">
-                Some Content
+                 &nbsp;
               </div>
             </div>
           </div>
 
           <div class="row">
             <div class="col-xs-12 col-md-3">
-            <h2>Title</h2>
+            <h2>h2 Title</h2>
             </div>
             <div class="col-xs-12 col-md-9">
+
               <div class="row">
-                <div class="col-md-4">
-                  <h2>Some Content</h2>
+                <div class="col-xs-12 col-md-4">
+                  <h2>h2 Content Title</h2>
                 </div>
                 <div class="col-xs-12 col-md-8">
-                  Las bufandas son útiles para mantenerte caliente, también tienen estilo. Solo necesito tirarme desde un avión con una bufanda para no coger un catarro. Mi perro es negro, mi rata es blanca
+                 <p>Las bufandas son útiles para mantenerte caliente, <a href="#">text link</a> también tienen estilo. Solo necesito tirarme desde un avión con una bufanda para no coger un catarro. Mi perro es negro, mi rata es blanca</p>
                 </div>
               </div>
 
               <div class="row">
-                <div class="col-md-4">
-                  <h2>Some Content</h2>
+                <div class="col-xs-12 col-md-4">
+                  <h2>h2 Title</h2>
                 </div>
                 <div class="col-xs-12 col-md-8">
-                  Las bufandas son útiles para mantenerte caliente, también tienen estilo. Solo necesito tirarme desde un avión con una bufanda para no coger un catarro. Mi perro es negro, mi rata es blanca
+                  <p>Las bufandas son útiles para mantenerte caliente, también tienen estilo. <span style={{textDecoration: 'underline'}}>Solo necesito tirarme desde un avión</span> con una bufanda para no coger un catarro. Mi perro es negro, mi rata es blanca</p>
                 </div>
               </div>
 
               <div class="row">
-                <div class="col-md-4">
-                  <h2>Some Content</h2>
+                <div class="col-xs-12 col-md-4">
+                  <h2>h2 Title</h2>
                 </div>
                 <div class="col-xs-12 col-md-8">
-                  Las bufandas son útiles para mantenerte caliente, también tienen estilo. Solo necesito tirarme desde un avión con una bufanda para no coger un catarro. Mi perro es negro, mi rata es blanca
+                  <div class="row">
+                    <div class="col-xs-12 col-md-6">
+                      <p>Unordered List</p>
+                      <ul>
+                        <li>List Item</li>
+                        <li>List Item</li>
+                        <li>List Item</li>
+                        <li>List Item</li>
+                      </ul>
+                    </div>
+                    <div class="col-xs-12 col-md-6">
+                      <p>Ordered List</p>
+                      <ol>
+                        <li>List Item</li>
+                        <li>List Item</li>
+                        <li>List Item</li>
+                        <li>List Item</li>
+                      </ol>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
