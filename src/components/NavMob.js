cf0a3636d19b137b6b5d057cbe257a006926f54a
Roman <roman@overhaulmedia.com>
2021-09-08 10:49:42 -0600

    adding globla styles and flexbox grid

--- src/components/NavMob.js
+++ src/components/NavMob.js
@@ -4,7 +4,7 @@ import { Component } from "react";
 
 import OmLogo from '../components/OmLogo'
 
-class NavDesk extends Component {
+class NavMob extends Component {
 
   constructor() {
         super()
@@ -76,43 +76,5 @@ class NavDesk extends Component {
   }
 }
 
-export default NavDesk
+export default NavMob
 
-// class BurgerMenu extends Component {
-
-//     constructor() {
-//         super()
-//         this.state = {
-//           menuIsOpen: false
-//         }
-//         this.toggleMenu = this.toggleMenu.bind(this)
-//     }
-
-//     toggleMenu() {
-
-//         this.setState( 
-//             function(prevState){
-//                 return{
-//                   menuIsOpen: !prevState.menuIsOpen
-//                 }
-//             }
-//          )
-//     }
-
-//     render() {
-//         return (
-//           <div
-//             onClick={this.toggleMenu}
-//             onKeyDown={this.toggleMenu}
-//             className={`nav-burger ${this.state.menuIsOpen ? 'open' : ''}`}
-//             id="navBurger"
-//             role="navigation"
-//           >
-//             <span></span>
-//             <span></span>
-//             <span></span>
-//           </div>
-//         )
-    
-//     }
-// }
