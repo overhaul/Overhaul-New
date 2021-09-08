6682f0654a87aedd3a23aa883856e5fa910608bc
Vincent Smuda <vincent.smuda@lluvia.studio>
2021-09-08 14:55:51 -0600

    Adds bootstrap styles

--- src/components/FixedSlide.js
+++ src/components/FixedSlide.js
@@ -14,12 +14,16 @@ class FixedSlide extends Component {
             />
             <div className="fixed-slide__content">
               <div className="container">
-                <h1 className="fixed-slide__title">
-                  {title}
-                </h1>
-                <a className="fixed-slide__link" href={link}>
-                  View Project
-                </a>
+                <div class="row">
+                  <div class="col-xs-12 col-sm-6 col-md-6 start-xs">
+                    <h1 className="fixed-slide__title ">
+                      {title}
+                    </h1>
+                    <a className="fixed-slide__link" href={link}>
+                      View Project
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
