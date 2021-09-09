fe6fd88c618340296101e1a661875e3202dbf9a2
Roman <roman@overhaulmedia.com>
2021-09-09 13:42:16 -0600

    full-width iamge and additon of some styles for dark mode for FullWidhtImage and PageTitle components

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,7 @@ import * as React from 'react'
 import Layout from '../components/Layout'
 import PageHero from '../components/PageHero'
 import PageTitle from '../components/PageTitle'
+import FullWidthImage from '../components/FUllWidthImage'
 
 const AboutPage = () => {
     return (
@@ -14,6 +15,10 @@ const AboutPage = () => {
            <PageTitle
             title="this is a Title Test"
             subTitle="Subtitle Test too,"
+            />
+            <FullWidthImage
+            image="https://images.unsplash.com/photo-1583483425070-cb9ce8fc51b5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3889&q=80"
+            alt="This is an Image i found on The internet"
             />
         </Layout>
     )
