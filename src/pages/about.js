89c91ffda622e4a2e2ec40dc0c69f300d2d5c50b
Roman <roman@overhaulmedia.com>
2021-09-09 10:24:57 -0600

    finishing touches on the hero - still needs responsive image

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ import PageHero from '../components/PageHero'
 const AboutPage = () => {
     return (
         <Layout>
-         <PageHero image='../images/corporate-services-3.jpg' title='This is the Title' subTitle="This will be the Subtitle"/>
+         <PageHero image='https://images.unsplash.com/photo-1631116618155-6074e787a30b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3150&q=80' title='This is the Title' subTitle="This will be the Subtitle"/>
         </Layout>
     )
 }
