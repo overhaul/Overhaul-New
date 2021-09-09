9edde493e8d5bdc8fc642482de167f024e29fbed
Vincent Smuda <vincent.smuda@lluvia.studio>
2021-09-09 11:35:22 -0600

    Creates the homepage typing slide

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,23 @@
-
 import * as React from 'react'
 import Layout from '../components/Layout'
 import FixedSlides from '../components/FixedSlides'
+import TypingSlide from '../components/TypingSlide'
 
-// Temporary content
+// Temporary content,
+// delete once pulling from site
 import slides from '../../mock-data/fixed-slides'
 
 const IndexPage = () => {
+
+  const titleSlideText = [
+    'You have this long to make an impression.',
+    'Stand out,',
+    'or sit on the bench.',
+  ]
+
   return (
     <Layout pageTitle="Home Page">
+      <TypingSlide text={titleSlideText} />
       <FixedSlides slides={slides} />
     </Layout>
   )
