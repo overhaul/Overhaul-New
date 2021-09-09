a7a64531c22b16e6f74f045e05ab57c6fd5dab4c
Roman <roman@overhaulmedia.com>
2021-09-09 15:54:29 -0600

    dual images from seperate file in mock-work folder, dark theme styles

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,12 +5,12 @@ import { StaticImage } from 'gatsby-plugin-image'
 const ContactPage = () => {
     return (
         <Layout pageTitle="Contact Us">
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-      <StaticImage
-        alt="Corporate flower needs"
-        src="../images/hero-corporate.jpg"
-      />
-    </Layout>
+            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+            <StaticImage
+                alt="Corporate flower needs"
+                src="../images/hero-corporate.jpg"
+            />
+        </Layout>
     )
 }
 
