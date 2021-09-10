81ccf89ef0f614ae481b2b4199821d80bfe862c6
Roman <roman@overhaulmedia.com>
2021-09-10 12:17:33 -0600

    footer alignment bugs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,22 +7,22 @@ class Footer extends React.Component {
         return (
             <footer className="footer">
                 <div className="container row">
-                    <div className="col-md-6 copyright">
+                    <div className="col-xs-12 col-sm-12 col-md-6 copyright">
                        <p>© 2021 Overhaul Media</p>
                     </div>
-                    <div className="col-md-6 row">
-                        <div className="col-md-4">
-                            <p>{ address }</p>
+                    <div className="col-xs-12 col-sm-12 col-md-6 row">
+                        <div className="col-xs-12 col-sm-12 col-md-4">
+                            <div dangerouslySetInnerHTML={{ __html:address}}/>
                         </div>
-                        <div className="col-md-4">
+                        <div className="col-xs-12 col-md-4">
                             <p><a href={'tel:' + phone} rel="noreferrer" target="_blank">{phone}</a></p>
                             <p><a href={'mailto:' + email} rel="noreferrer" target="_blank">{ email }</a></p>
                         </div>
-                        <div className="col-md-2">
+                        <div className="col-xs-12  col-md-2">
                             <Link to="/careers">Careers</Link>
                             <Link to="/services">Services</Link>
                         </div>
-                         <div className="col-md-2">
+                         <div className="col-xs-12 col-md-2">
                             <a href="https://www.instagram.com/overhaulmedia/?hl=en" target="_blank" rel="noreferrer">Instagram</a>
                             <a href="https://ca.linkedin.com/company/overhaul-media" target="_blank" rel="noreferrer">Linkedin</a>
                         </div>
