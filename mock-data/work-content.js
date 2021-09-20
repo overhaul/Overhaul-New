6bde040291dd6163c7fbad190601dc7d166ab158
Roman <roman@overhaulmedia.com>
2021-09-20 16:08:24 -0600

    initial commit of the clients component

--- mock-data/work-content.js
+++ mock-data/work-content.js
@@ -1,3 +1,11 @@
+import Cbc from '../src/images/clients/logo-cbc.png'
+import Haven from '../src/images/clients/logo-haven.png'
+import Naked from '../src/images/clients/logo-naked.png'
+import Syncrude from '../src/images/clients/logo-syncrude.png'
+import Alberta from '../src/images/clients/logo-alberta.png'
+import Edmonton from '../src/images/clients/logo-edmonton.png'
+import Katz from '../src/images/clients/logo-katz.png'
+
 export const images = [
     {
         src: "https://images.unsplash.com/photo-1631116618155-6074e787a30b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3150&q=80",
@@ -33,4 +41,35 @@ export const listItems =[
         title: 'Enterprise.',
     },
 
+]
+
+export const clients =[
+    {
+        src: Haven,
+        alt: 'Haven'
+    },
+    {
+        src: Cbc,
+        alt: 'Canadian Broadcasting Corporation'
+    },
+    {
+        src: Syncrude,
+        alt: 'Syncrude'
+    },
+    {
+        src: Naked,
+        alt: 'Naked and Famous'
+    },
+    {
+        src: Katz,
+        alt: 'Katz Group'
+    },
+    {
+        src: Edmonton,
+        alt: 'City of Edmonton'
+    },
+    {
+        src: Alberta,
+        alt: 'Province of Alberta'
+    },
 ]
