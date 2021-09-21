eb7f4550deabe086b4a50410892524bccebda9b0
Roman <roman@overhaulmedia.com>
2021-09-21 11:50:20 -0600

    awards component first commit

---
+++ mock-data/work-awards.js
@@ -0,0 +1,69 @@
+export const awards =[
+    {
+        title: 'ACE Award - Winner, Best in Show',
+        year: "2021",
+        project:"Love AB Forests",
+        link:"#"
+    },
+    {
+        title: 'ACE Award - Winner, Not-for-Profit Campaign',
+        year: "2021",
+        project:"Love AB Forests",
+        link:"#"
+    },
+    {
+        title: 'ACE Award - Winner, Innovative Use of Technology',
+        year: "2021",
+        project:"Love AB Forests",
+        link:"#"
+    },
+    {
+        title: 'ACE Award - Winner, Not-for-Profit Microsite',
+        year: "2021",
+        project:"Love AB Forests",
+        link:"#"
+    },
+    {
+        title: 'ACE Award - Winner, Innovation',
+        year: "2021",
+        project:"Love AB Forests",
+        link:"#"
+    },
+    {
+        title: 'ACE Award - Distinction, Not-for-Profit Audio Series',
+        year: "2021",
+        project:"Love AB Forests",
+        link:"#"
+    },
+    {
+        title: 'Awwards, Honorable Mention',
+        year: "2020",
+        project:"Canada ICI",
+        link:"#"
+    },
+    {
+        title: 'Awwards, Honorable Mention',
+        year: "2020",
+        project:"GoldSquare",
+        link:"#"
+    },
+    {
+        title: 'Applied Arts, Digital – Single Award',
+        year: "2016",
+        project:"YEG City Budget",
+        link:"#"
+    },
+    {
+        title: 'Alberta Film & Television Award - Best Cross Platform Production',
+        year: "2015",
+        project:"Project",
+        link:"#"
+    },
+    {
+        title: 'ACE Award - Not for Profit Interactive',
+        year: "2012",
+        project:"Share Hope",
+        link:"#"
+    },
+    
+]
