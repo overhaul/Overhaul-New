ad66292acdc4d83efcab5f9fef39136a905ce930
Roman <roman@overhaulmedia.com>
2021-09-22 10:41:50 -0600

    Areas of Expertise Block, not finished

---
+++ mock-data/work-expertise.js
@@ -0,0 +1,15 @@
+import HAVEN from '../src/images/clients/haven-feature.jpg'
+
+export const areas =[
+	{
+		title: 'Lifestyle, fashion, and beauty.',
+		paragraph: '<p>Las bufandas son útiles para mantenerte caliente, también tienen estilo. Solo necesito tirarme desde un avión con una bufanda para no coger un catarro. Mi perro es negro, mi rata es blanca</p>',
+		client: {
+			title: 'Haven',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+			},
+		},
+	},
+]
