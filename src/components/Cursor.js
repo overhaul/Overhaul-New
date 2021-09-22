b96f47d40e321a096c05d3af9eac60ae2cf1f97d
Roman <roman@overhaulmedia.com>
2021-09-22 16:27:17 -0600

    adding the custom cursor functionality follwing a tutorial

---
+++ src/components/Cursor.js
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import useMousePosition from "../hooks/useMousePosition";
+import { MouseContext } from "../context/mouse-context";
+
+const Cursor = () => {
+  const { cursorType, cursorChangeHandler } = useContext(MouseContext);
+  const { x, y } = useMousePosition();
+  return (
+    <>
+      <div
+        style={{ left: `${x}px`, top: `${y}px` }}
+        className={"cursor " + cursorType}
+      ></div>
+      <div
+        className={"dot " + cursorType}
+        style={{ left: `${x}px`, top: `${y}px` }}
+      ></div>
+    </>
+  );
+};
+export default Cursor
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,9 @@
 import * as React from 'react'
+import { useContext } from "react"
+import { MouseContext } from "../context/mouse-context"
+import MouseContextProvider from "../context/mouse-context";
+import Cursor from '../components/Cursor'
+
 import '../styles/layout.scss'
 import '../styles/styles.scss'
 
@@ -8,22 +13,27 @@ import Footer from '../components/Footer'
 
 const Layout = ({ children }) => {
 
+  const { cursorType, cursorChangeHandler } = useContext(MouseContext)
   return (
     <div>
-      <header>
-        <NavDesk/>
-        <NavMob/>
-      </header>
-      <main>
-        {children}
-      </main>
-      <Footer
-        address="<p>7347 104 Street NW</p><p>Edmonton, AB T6E 4B9</p>"
-        phone="780 758 8642"
-        email="hello@overhaulmedia.com"
-        />
+      <MouseContextProvider>
+        <header>
+          <NavDesk/>
+          <NavMob/>
+        </header>
+        <Cursor/>
+        <main>
+          {children}
+        </main>
+        <Footer
+          address="<p>7347 104 Street NW</p><p>Edmonton, AB T6E 4B9</p>"
+          phone="780 758 8642"
+          email="hello@overhaulmedia.com"
+          />
+      </MouseContextProvider>
     </div>
   )
 }
+
 
 export default Layout
