de37ed105a416b1205f7631297873832e824ff6b
Roman <roman@overhaulmedia.com>
2021-09-22 12:04:04 -0600

    Figured out the nested map

--- src/components/BlockExpertise.js
+++ src/components/BlockExpertise.js
@@ -22,7 +22,7 @@ class BlockExpertise extends React.Component {
          )
     }
     render() {
-        const { title, areas } = this.props
+        const { title, paragraph, areas } = this.props
         return (
             <div className="block-expertise">
                 <div className="container row">
@@ -31,12 +31,12 @@ class BlockExpertise extends React.Component {
                         <h2>{ title }</h2>
                         <div onClick={this.toggleAccordion} className={`close ${this.state.accordionIsOpen ? 'open' : ''}`}></div>
                     </div>
-                    <div className={`block-expertise_content accordion col-md-9 col-xs-12 ${this.state.accordionIsOpen ? 'open' : ''}`}>
-                        {areas.map((area, client, index) => (
-                            <div key={index}>
-                                <h2 className="col-xs-12 col-md-3">{area.title}</h2>
-                                 { area.paragraph ? <p className="col-md-9" dangerouslySetInnerHTML={{ __html:area.paragraph}}></p> : ''}
-                                 <ul className="col-xs-12 col-md-9 offset-md-3">
+                    <div className={`accordion col-md-9 col-xs-12 ${this.state.accordionIsOpen ? 'open' : ''}`}>
+                        {areas.map(({ title, paragraph, client, index}) => (
+                            <div className="row block-expertise_content" key={index}>
+                                <h2 className="col-xs-12 col-md-4">{title}</h2>
+                                { paragraph ? <p className="col-md-8" dangerouslySetInnerHTML={{ __html:paragraph}}></p> : ''}
+                                <ul className="col-xs-12 col-md-8 col-md-offset-4">
                                     {client.map((client, index) => (
                                         <li key={index}>{client.title}
                                         <img className="block-expertise_content-image" 
@@ -44,7 +44,7 @@ class BlockExpertise extends React.Component {
                                              alt={client.image.alt}/>
                                         </li>
                                     ))}
-                                 </ul>
+                                </ul>
                             </div>
                         ))}
                     </div>
