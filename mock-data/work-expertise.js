79e378503da90a584836f9478eff92c89eac26ab
Roman <roman@overhaulmedia.com>
2021-09-22 12:04:04 -0600

    Figured out the nested map

--- mock-data/work-expertise.js
+++ mock-data/work-expertise.js
@@ -4,12 +4,331 @@ export const areas =[
 	{
 		title: 'Lifestyle, fashion, and beauty.',
 		paragraph: '<p>Las bufandas son útiles para mantenerte caliente, también tienen estilo. Solo necesito tirarme desde un avión con una bufanda para no coger un catarro. Mi perro es negro, mi rata es blanca</p>',
-		client: {
+		client: [
+			{
 			title: 'Haven',
 			image: {
 				src: HAVEN,
 				alt: 'Haven Menswear Canada'
+				},
 			},
-		},
+			{
+			title: 'Helm',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Goldsquare',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Fancii',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Healthy Bedroom',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Orijen',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'SOHO Hair',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Publish Brand',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Naked and Famous',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			}
+		]
+	},
+	{
+		title: 'Food, drink, and hospitality.',
+		paragraph: 'Often working alongside interior designers, architects, and owners, we help shape place and create a unique experience through various forms of design. ',
+		client: [
+			{
+			title: 'Uptop',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Farrow',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Three Boars',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'HighDough',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Pressd',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'North 53',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Wishbone',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Baiju',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Malt and Mortar',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			}
+		]
+	},
+	{
+		title: 'Commercial real estate (CRE), property and asset management. ',
+		paragraph: 'Creating desire, adding value, placemaking for meaningful relationships with the built environment. ',
+		client: [
+			{
+			title: 'Canada ICI',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Epic Investment Services',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'HSBC Place',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Currents of Windermere',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Beljan',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Hyphen Group',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: '103 Street Center',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'North District',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'DOSC',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			}
+		]
+	},
+	{
+		title: 'Arts, culture, and media.',
+		paragraph: 'As creatives we feel it’s important to support the arts and culture. Helping artists, storytellers tell their story and express themselves in a new way.',
+		client: [
+			{
+			title: 'Theater Network',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Georgie Magazine',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Human Odyssey',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Handful of Films',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			}
+		]
+	},
+	{
+		title: 'Politics, advertising and PR',
+		paragraph: 'Wether its a site for a national campaign about STD’s, forests in Alberta or our beloved Edmonton’s annual budget. We’ve worked in politics, arms length government orgnaizations.',
+		client: [
+			{
+			title: 'Love AB Forests VR Tour',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'YEG City Budget',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Report to the City',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Invest Alberta Corporation',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Alberta Forest Products Association',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'City of Leduc',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			}
+		]
+	},
+	{
+		title: 'Professional, healthcare and medical',
+		paragraph: 'Old school professions are rapidly evolving in practice. We’re well equipt to help navigate though that transition into today’s modern world. Be it technically, strategically or visually.',
+		client: [
+			{
+			title: 'Cure MD',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'SMP',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Mettra Pharmacy',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Pure Aligners',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			},
+			{
+			title: 'Hello Dental',
+			image: {
+				src: HAVEN,
+				alt: 'Haven Menswear Canada'
+				},
+			}
+		]
 	},
 ]
