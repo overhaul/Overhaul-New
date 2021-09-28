7964672d069e07733f6615e7fdee2b92c4c85e45
Roman <roman@overhaulmedia.com>
2021-09-28 11:25:40 -0600

    adding projects to the work page, building the work pages

---
+++ work-data/UptopImages.js
@@ -0,0 +1,62 @@
+import image from '../src/images/clients/uptop/Uptop1.jpg'
+import image1 from '../src/images/clients/uptop/Uptop2.jpg'
+import image2 from '../src/images/clients/uptop/Uptop3.png'
+import image3 from '../src/images/clients/uptop/Uptop4.jpg'
+import image4 from '../src/images/clients/uptop/Uptop5.jpg'
+import image5 from '../src/images/clients/uptop/Uptop6.jpg'
+import image6 from '../src/images/clients/uptop/Uptop7.jpg'
+import image7 from '../src/images/clients/uptop/Uptop8.jpg'
+import image8 from '../src/images/clients/uptop/Uptop9.jpg'
+import image9 from '../src/images/clients/uptop/Uptop11.jpg'
+import image10 from '../src/images/clients/uptop/Uptop12.jpg'
+import image11 from '../src/images/clients/uptop/Uptop13.jpg'
+import image12 from '../src/images/clients/uptop/Uptop14.jpg'
+import image13 from '../src/images/clients/uptop/Uptop15.jpg'
+
+export const UtFwOne = image
+export const UtFwTwo = image1
+export const UtFwThree = image4
+export const UtFwFour = image9
+export const UtFwFive = image12
+export const UtFwSix = image13
+
+export const UtDiOne = [
+	{
+		src: image2,
+		alt: 'Uptop Branding'
+	},
+	{
+		src: image3,
+		alt: 'Uptop Branding'
+	}
+]
+export const UtDiTwo = [
+	{
+		src: image5,
+		alt: 'Uptop Branding'
+	},
+	{
+		src: image6,
+		alt: 'Uptop Branding'
+	}
+]
+export const UtDiThree = [
+	{
+		src: image7,
+		alt: 'Uptop Branding'
+	},
+	{
+		src: image8,
+		alt: 'Uptop Branding'
+	}
+]
+export const UtDiFour = [
+	{
+		src: image10,
+		alt: 'Uptop Branding'
+	},
+	{
+		src: image11,
+		alt: 'Uptop Branding'
+	}
+]
