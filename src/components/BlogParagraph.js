bc1c42512dc2262d10579e46015e80ec1024fb11
roman-overhaul <64929409+roman-overhaul@users.noreply.github.com>
2021-10-01 15:46:02 -0600

    Update BlogParagraph.js
    adding a "2"

--- src/components/BlogParagraph.js
+++ src/components/BlogParagraph.js
@@ -4,7 +4,7 @@ class BlogParagraph extends React.Component {
     
     render() {
         return (
-            <div className="blog-content col-md-8 col-xs-1" dangerouslySetInnerHTML={{ __html:this.props.content}}></div>
+            <div className="blog-content col-md-8 col-xs-12" dangerouslySetInnerHTML={{ __html:this.props.content}}></div>
           )
       }
 }
