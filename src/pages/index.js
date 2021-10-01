23c903baae67993998eea8f23e9bd4c364806105
Roman <roman@overhaulmedia.com>
2021-10-01 16:10:14 -0600

    abstracting the height of the TypeSlide

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,12 +13,12 @@ const IndexPage = () => {
     'You have this long to make an impression.',
     'Stand out,',
     'or sit on the bench.',
-    'get', 'to', 'zeee', 'choppa',
+    // 'get', 'to', 'zeee', 'choppa',
   ]
 
   return (
-    <Layout pageTitle="Home Page">
-      <TypingSlide text={titleSlideText} />
+    <Layout themeColor="dark" pageTitle="Home Page">
+      <TypingSlide height="300" text={titleSlideText} />
       <FixedSlides slides={slides} />
     </Layout>
   )
