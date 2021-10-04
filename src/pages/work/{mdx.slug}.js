1796d08f4775d950ed185a244ac586f7bcbb0d9a
Roman <roman@overhaulmedia.com>
2021-10-04 16:09:42 -0600

    adding animations to the images on the work pages

--- src/pages/work/{mdx.slug}.js
+++ src/pages/work/{mdx.slug}.js
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React, {useRef, useEffect} from 'react'
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 
@@ -6,27 +6,55 @@ import PageHero from '../../components/PageHero'
 import WorkIntro from '../../components/WorkIntro'
 import Layout from '../../components/Layout'
 
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
 const WorkPost = ({ data }) => {
-   if(!data.mdx?.frontmatter) return ''
+  const el = useRef();
+
+  useEffect(() => {
+
+    const q = gsap.utils.toArray(".gsap-fade-in");
+    for(let i = 0; i < q.length; i++) {
+      console.log(q[i])
+      gsap.fromTo(q[i], {
+        opacity: 0,
+      }, {
+        scrollTrigger: {
+          trigger: q[i],
+          start: 'top bottom',
+          end: '+=500',
+          scrub: 0.5,
+        },
+        opacity: 1,
+        duration: 1,
+      });
+    }
+  }, []);
+
+  if(!data.mdx?.frontmatter) return ''
   const { title, subtitle, paragraph, lists, hero_image } = data.mdx.frontmatter
 
-    return (
-        <Layout pageTitle={title}>
-        <PageHero
-          title={title}
-          subTitle={subtitle}
-          image={hero_image.publicURL}
-        />
-        <WorkIntro
-          title={subtitle}
-          paragraph={paragraph}
-          taxonomies={lists}
-        />
-        <MDXRenderer>
+  return (
+    <Layout pageTitle={title}>
+      <PageHero
+        title={title}
+        subTitle={subtitle}
+        image={hero_image.publicURL}
+      />
+      <WorkIntro
+        title={subtitle}
+        paragraph={paragraph}
+        taxonomies={lists}
+      />
+      <div ref={el}>
+        <MDXRenderer >
           {data.mdx.body}
         </MDXRenderer>
-      </Layout>
-    )
+      </div>
+    </Layout>
+  )
 }
 
 export const query = graphql `
