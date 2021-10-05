10b224cad8d13395d32d560a95cc1668c6076c9b
Vincent Smuda <vincent.smuda@lluvia.studio>
2021-10-05 13:01:50 -0600

    Links wordpress posts to gatsby

--- src/pages/work/index.js
+++ src/pages/work/index.js
@@ -39,11 +39,11 @@ class WorkPage extends Component {
     if (this.state.currentCategory === -1) return true
     const currentCategoryObject = categories[this.state.currentCategory]
     const currentCategoryTitle = currentCategoryObject?.title
-    return node.frontmatter.cats.find((cat) => cat.title === currentCategoryTitle)
+    return (node.cats || []).find((cat) => cat.title === currentCategoryTitle)
   }
 
   render() {
-    var { data } = this.props;
+    const { nodes: workNodes } = this.props.data.allWpPost;
     return (
         <Layout>
           <PageTitle
@@ -95,40 +95,50 @@ class WorkPage extends Component {
           </div>
 
           <div className={`work container row grid ${this.state.listIsOpen ? 'grid-view' : 'list-view'}`}>
-           {
-            data.allMdx.nodes.filter(this.fitlerNode).map((node, index) => (
-              <div className="col-xs-12 col-md-6 grid_card" key={index}>
-                <article className="work_card" key={node.id}>
-                  <Link to={`/work/${node.slug.toLowerCase()}`}>
-                    <div className="work_card-featured-image">
-                      <img src={node.frontmatter.hero_image.publicURL} alt={node.frontmatter.hero_image_alt}/>
-                    </div>
-                    <p>{node.frontmatter.title}</p>
-                    <p className="work_card-subtitle">{node.frontmatter.subtitle}</p>
-                  </Link>
-                </article>
-              </div>
-            ))
-          }
+            {
+              workNodes.filter(this.fitlerNode).map((node, index) => {
+                return (
+                <div className="col-xs-12 col-md-6 grid_card" key={index}>
+                  <article className="work_card" key={node.id}>
+                    <Link to={`/work/${node.slug.toLowerCase()}`}>
+                      <div className="work_card-featured-image">
+                        {node.featuredImage && <img
+                          src={node.featuredImage.node.sourceUrl}
+                          alt={node.featuredImage.node.altText}
+                        />}
+                      </div>
+                      <p>{node.title}</p>
+                      <p className="work_card-subtitle">{node.subtitle || 'NO SUBTITLE'}</p>
+                    </Link>
+                  </article>
+                </div>
+              )
+              })
+            }
           </div>
+
           <div className={`work container row list ${this.state.listIsOpen ? 'grid-view' : 'list-view'}`}>
-             {
-              data.allMdx.nodes.filter(this.fitlerNode).map((node, index) => (
+            {
+              workNodes.filter(this.fitlerNode).map((node, index) => (
                 <div className="work_card-list col-xs-12" key={index}>
                   <article key={node.id}>
                     <Link to={`/work/${node.slug.toLowerCase()}`}>
-                      <p>{node.frontmatter.title}</p>
-                      <p className="work_card-list-subtitle">{node.frontmatter.subtitle}</p>
-                      <p className="work_card-list-date">{node.frontmatter.date}</p>
+                      <p>{node.title}</p>
+                      <p className="work_card-list-subtitle">{node.subtitle}</p>
+                      <p className="work_card-list-date">{node.date}</p>
                     </Link>
                   </article>
                   <div className="work_card-list-image">
-                    <img src={node.frontmatter.hero_image.publicURL} alt={node.frontmatter.hero_image_alt}/>
+                    {node.featuredImage && <img
+                      src={node.featuredImage.node.sourceUrl}
+                      alt={node.featuredImage.node.altText}
+                    />}
                   </div>
                 </div>
               ))
             }
           </div>
+
           <BlockCallToAction
             title='Look like something you need?'
             cta='Start the conversation.'
@@ -140,26 +150,27 @@ class WorkPage extends Component {
 }
 
 export const query = graphql `
-  query WorkPage{
-    allMdx(sort: {fields: frontmatter___date, order: DESC},filter: {fileAbsolutePath: {regex: "/work/"}}) {
+  query WorkPage {
+    allWpPost {
       nodes {
-        frontmatter {
-          cats {
-            title
-          }
-          title
-          subtitle
-          paragraph
-          hero_image_alt
-          hero_image {
-            publicURL
-          }
-          date(formatString: "YYYY")
-        }
         id
+        uri
         slug
+        title
+        excerpt
+        content
+        featuredImage {
+          node {
+            srcSet
+            slug
+            sourceUrl
+            title
+            uri
+          }
+        }
       }
     }
   }
 `
+
 export default WorkPage
