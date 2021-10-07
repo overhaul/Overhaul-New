9ec5837a1e617721da6277be74a72c9c03f5bc21
Vincent Smuda <vincent.smuda@lluvia.studio>
2021-10-07 14:22:08 -0600

    removes inline styles from gut component

--- src/components/GutenbergContent.js
+++ src/components/GutenbergContent.js
@@ -1,28 +1,26 @@
 import React from 'react'
 import Sections from './Sections'
 
-function GutenbergContent ({ content }) {
+function GutenbergContent ({ content, containerClass }) {
   const sectionBlocks = content.match(/<!--SECTIONS(.+)ENDSECTIONS-->/g)
   const gutenbergBlocks =
     content.replace(/<!--SECTIONS(.+)ENDSECTIONS-->/g, 'BLOCKSPLITGUTENBERGSECTIONSBLOCKSPLIT')
     .split('BLOCKSPLIT')
     .filter((v) => !v.match(/^[\s\n]*$/))
   return (
-    <div style={{zIndex: 100, backgroundColor: 'white', position: 'relative', willChange: 'transform'}}>
-      <div className="container">
-        {gutenbergBlocks.map((content, index) => {
+    <div className={'gutenberg-content ' + (containerClass ?? 'container')}>
+      {gutenbergBlocks.map((content, index) => {
 
-          if (content.match(/GUTENBERGSECTIONS/) && sectionBlocks.length) {
-            const sections = JSON.parse(sectionBlocks.shift().replace(/<!--SECTIONS|ENDSECTIONS-->/g, ''))
-            return (<Sections sections={sections.sections} key={index} />)
-          }
-          
-          return (
-            <div dangerouslySetInnerHTML={{__html: content}} key={index}>
-            </div>
-          )
-        })}
-      </div>
+        if (content.match(/GUTENBERGSECTIONS/) && sectionBlocks.length) {
+          const sections = JSON.parse(sectionBlocks.shift().replace(/<!--SECTIONS|ENDSECTIONS-->/g, ''))
+          return (<Sections sections={sections.sections} key={index} />)
+        }
+        
+        return (
+          <div dangerouslySetInnerHTML={{__html: content}} key={index}>
+          </div>
+        )
+      })}
     </div>
   )
 }
