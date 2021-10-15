747d010e2767cd303febe020d40107a711f6359b
Roman <roman@overhaulmedia.com>
2021-10-15 15:05:07 -0600

    fixing non-existing image error

--- src/pages/work/index.js
+++ src/pages/work/index.js
@@ -67,86 +67,86 @@ class WorkPage extends Component {
   render() {
     const { nodes: workNodes } = this.props.data.allWpPost;
     return (
-        <Layout seo={this.props.data.wpPage.seo}>
-          <PageTitle
-            title='Building brands that matter.'
-          />
-          <div className="container row">
-            <div className="col-xs-12 work-filters">
-              <div className="work-filter">
-                <ul className="work-filter_items">
-                  <li className="work-filter_item first">
-                    <a onClick={ () => this.updateCategory(-1)}>
-                      {this.categoryList()[this.state.currentCategory]?.title ?? 'All'}
-                    </a>
+      <Layout seo={this.props.data.wpPage.seo}>
+        <PageTitle
+          title='Building brands that matter.'
+        />
+        <div className="container row">
+          <div className="col-xs-12 work-filters">
+            <div className="work-filter">
+              <ul className="work-filter_items">
+                <li className="work-filter_item first">
+                  <a onClick={ () => this.updateCategory(-1)}>
+                    {this.categoryList()[this.state.currentCategory]?.title ?? 'All'}
+                  </a>
+                </li>
+                <li className={'work-filter_item ' + (this.state.currentCategory === -1 ? 'work-filter_item--off' : '')}>
+                  <a onClick={ () => this.updateCategory(-1)}>All</a>
+                </li>
+                {this.categoryList().map((category, index) => (
+                  <li className={'work-filter_item ' + (this.state.currentCategory === index ? 'work-filter_item--off' : '')} key={index}>
+                    <a onClick={ () => this.updateCategory(index)}>{category.title}</a>
                   </li>
-                  <li className={'work-filter_item ' + (this.state.currentCategory === -1 ? 'work-filter_item--off' : '')}>
-                    <a onClick={ () => this.updateCategory(-1)}>All</a>
-                  </li>
-                  {this.categoryList().map((category, index) => (
-                    <li className={'work-filter_item ' + (this.state.currentCategory === index ? 'work-filter_item--off' : '')} key={index}>
-                      <a onClick={ () => this.updateCategory(index)}>{category.title}</a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className="work-toggle">
-                <p className="work-toggle_trigger">{this.state.listIsOpen ? 'Grid' : 'List'}</p>
-                <div className="work-toggle_buttons">
-                  {this.state.listIsOpen ? 
-                    <button className="work-toggle_button list"
-                        onClick={this.toggleView}
-                        onKeyDown={this.toggleView}
-                        >
-                        <p>List</p>
-                    </button> : 
-                    ''
-                  }
-                  {this.state.listIsOpen ? 
-                    '' : 
-                    <button className="work-toggle_button grid"
-                        onClick={this.toggleView}
-                        onKeyDown={this.toggleView}
-                        > 
-                        <p>Grid</p>
-                    </button>
-                  }
-                </div>
+                ))}
+              </ul>
+            </div>
+            <div className="work-toggle">
+              <p className="work-toggle_trigger">{this.state.listIsOpen ? 'Grid' : 'List'}</p>
+              <div className="work-toggle_buttons">
+                {this.state.listIsOpen ? 
+                  <button className="work-toggle_button list"
+                      onClick={this.toggleView}
+                      onKeyDown={this.toggleView}
+                      >
+                      <p>List</p>
+                  </button> : 
+                  ''
+                }
+                {this.state.listIsOpen ? 
+                  '' : 
+                  <button className="work-toggle_button grid"
+                      onClick={this.toggleView}
+                      onKeyDown={this.toggleView}
+                      > 
+                      <p>Grid</p>
+                  </button>
+                }
               </div>
             </div>
           </div>
+        </div>
 
-          <div className={`work container row grid ${this.state.listIsOpen ? 'grid-view' : 'list-view'}`}>
-            {
-              workNodes.filter(this.fitlerNode).map((node, index) => {
-                return (
-                  <WorkCard
-                    key={node.id}
-                    title={node.title}
-                    url={`/work/${node.slug.toLowerCase()}`}
-                    featuredImage={node.featuredImage}
-                    gatsbyImageData={node.featuredImage?.node.localFile.childImageSharp}
-                    excerpt={node.workSubtitle.subTitle}
-                  />
-                )
-              })
-            }
-          </div>
+        <div className={`work container row grid ${this.state.listIsOpen ? 'grid-view' : 'list-view'}`}>
+          {
+            workNodes.filter(this.fitlerNode).map((node, index) => {
+              return (
+                <WorkCard
+                  key={node.id}
+                  title={node.title}
+                  url={`/work/${node.slug.toLowerCase()}`}
+                  featuredImage={node.featuredImage}
+                  gatsbyImageData={node.featuredImage?.node.localFile.childImageSharp}
+                  excerpt={node.workSubtitle.subTitle}
+                />
+              )
+            })
+          }
+        </div>
 
-          <div className={`work container row list ${this.state.listIsOpen ? 'grid-view' : 'list-view'}`}>
-            {
-              workNodes.filter(this.fitlerNode).map((node, index) => (
-                <WorkListItem {...node} key={index} />
-              ))
-            }
-          </div>
+        <div className={`work container row list ${this.state.listIsOpen ? 'grid-view' : 'list-view'}`}>
+          {
+            workNodes.filter(this.fitlerNode).map((node, index) => (
+              <WorkListItem {...node} key={index} />
+            ))
+          }
+        </div>
 
-          <BlockCallToAction
-            title='Look like something you need?'
-            cta='Start the conversation.'
-            link='/contact'
-          />
-        </Layout>
+        <BlockCallToAction
+          title='Look like something you need?'
+          cta='Start the conversation.'
+          link='/contact'
+        />
+      </Layout>
     )
   }
 }
