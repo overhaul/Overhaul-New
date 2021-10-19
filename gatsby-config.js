3d92ad1d4c968b9def080b7af595511bc73dd704
Vincent Smuda <vincent.smuda@lluvia.studio>
2021-10-19 10:17:20 -0600

    Adds debug to preview

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,20 +4,21 @@ module.exports = {
     title: "The New Overhaul New",
   },
   plugins: [
-    // "gatsby-plugin-image",
     `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-wordpress`,
       options: {
-        url: process.env.WPGRAPHQL_URL || 'http://overhaul20.wpengine.com/graphql',    
+        url: process.env.WPGRAPHQL_URL || 'https://overhaul20.wpengine.com/graphql',
         html: {
           useGatsbyImage: true,
+        },
+        debug: {
+          preview: true,
         },
         useACF: true,
       },
     },
     "gatsby-plugin-image",
-    "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
