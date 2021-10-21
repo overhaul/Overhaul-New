db05ffc7b65e1333146b23ffc66c640721395082
Vincent Smuda <vincent.smuda@lluvia.studio>
2021-10-21 16:07:54 -0600

    hides noindex

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -124,8 +124,8 @@ function Layout({children, themeColor, pageTitle, seo, startNavWhite = false}) {
         {seo.canonical ? <link rel="canonical" href={seo.canonical} /> : ''}
         {seo.metaDesc ? <meta name="description" content={seo.metaDesc.replace(/<[^>]+>|\n|\r|\\n|\\r/g, '')} /> : ''}
         {seo.metaKeywords ? <meta name="keywords" content={seo.metaKeywords} /> : ''}
-        {seo.metaRobotsNofollow ? <meta name="robots" content={seo.metaRobotsNofollow} /> : ''}
-        {seo.metaRobotsNoindex ? <meta name="robots" content={seo.metaRobotsNoindex} /> : ''}
+        {/* {seo.metaRobotsNofollow ? <meta name="robots" content={seo.metaRobotsNofollow} /> : ''} */}
+        {/* {seo.metaRobotsNoindex ? <meta name="robots" content={seo.metaRobotsNoindex} /> : ''} */}
 
         <meta property="og:locale" content="en_US" />
         {seo.opengraphType ? <meta name="og:type" content={seo.opengraphType} /> : ''}
