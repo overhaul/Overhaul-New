5d82df9cdf67cfe96415547b50426d2fdf49357b
Roman <roman@overhaulmedia.com>
2021-10-25 11:37:49 -0600

    adding hreflang to links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,16 +15,16 @@ class Footer extends React.Component {
                             <div dangerouslySetInnerHTML={{ __html:address}}/>
                         </div>
                         <div className="col-xs-12 col-md-6 col-lg-4">
-                            <p><a href={'tel:' + phone} rel="noreferrer" target="_blank">{phone}</a></p>
-                            <p><a href={'mailto:' + email} rel="noreferrer" target="_blank">{ email }</a></p>
+                            <p><a href={'tel:' + phone} rel="noreferrer" target="_blank" hrefLang="en">{phone}</a></p>
+                            <p><a href={'mailto:' + email} rel="noreferrer" target="_blank" hrefLang="en">{ email }</a></p>
                         </div>
                         <div className="col-xs-12 col-md-6 col-lg-2">
-                            <Link to="/careers">Careers</Link>
-                            <Link to="/about#Services">Services</Link>
+                            <Link to="/careers" rel="alternate" hrefLang="en">Careers</Link>
+                            <Link to="/about#Services" rel="alternate" hrefLang="en">Services</Link>
                         </div>
                          <div className="col-xs-12 col-md-6 col-lg-2">
-                            <a href="https://www.instagram.com/overhaulmedia/?hl=en" target="_blank" rel="noreferrer">Instagram</a>
-                            <a href="https://ca.linkedin.com/company/overhaul-media" target="_blank" rel="noreferrer">LinkedIn</a>
+                            <a href="https://www.instagram.com/overhaulmedia/?hl=en" target="_blank" rel="noreferrer" hrefLang="en">Instagram</a>
+                            <a href="https://ca.linkedin.com/company/overhaul-media" target="_blank" rel="noreferrer" hrefLang="en">LinkedIn</a>
                         </div>
                     </div>
                 </div>
