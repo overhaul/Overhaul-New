022fa6f4a2e8c92b46007b65c6e39271e6d74e61
Roman <roman@overhaulmedia.com>
2022-01-10 14:38:04 -0700

    small css upadte to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,43 +2,43 @@ import * as React from 'react'
 import { Link } from 'gatsby'
 
 class Footer extends React.Component {
-    render() {
-        const { phone, email, address } = this.props
-        return (
-            <footer className="footer">
-                <div className="container row">
-									<div className="col-xs-12 col-sm-12 col-md-4  copyright">
-										<p>© 2021 Overhaul Media</p>
-									</div>
-									<div className="col-xs-12 col-sm-12 col-md-8 row nav-container">
-										<div class="col-md-6 nav">
-											<div className="mob-1">
-													<div itemProp="address" dangerouslySetInnerHTML={{ __html:address}}/>
-											</div>
-											<div className="mob-1">
-													<p><a itemProp="telephone" href={'tel:' + phone} rel="noreferrer" target="_blank" hrefLang="en">{phone}</a></p>
-													<p><a itemProp="email" href={'mailto:' + email} rel="noreferrer" target="_blank" hrefLang="en">{ email }</a></p>
-											</div>
-										</div>
-										<div class="col-md-6 nav">
-											<div className="mob-2">
-													<Link to="/ecommerce" rel="alternate" hrefLang="en">eCommerce</Link>
-													<Link to="/bar-restaurant-branding-edmonton" rel="alternate" hrefLang="en">Bar & Restaurant Branding</Link>
-											</div>
-											<div className="mob-2">
-													<Link to="/careers" rel="alternate" hrefLang="en">Careers</Link>
-													<Link to="/about#Services" rel="alternate" hrefLang="en">Services</Link>
-											</div>
-											<div className="mob-2">
-												<a href="https://www.instagram.com/overhaulmedia/?hl=en" target="_blank" rel="noreferrer" hrefLang="en">Instagram</a>
-												<a href="https://ca.linkedin.com/company/overhaul-media" target="_blank" rel="noreferrer" hrefLang="en">LinkedIn</a>
-											</div>
-										</div>
-									</div>
-                </div>
-            </footer>
-        )
-    }
+	render() {
+		const { phone, email, address } = this.props
+		return (
+			<footer className="footer">
+					<div className="container row">
+						<div className="col-xs-12 col-sm-12 col-md-4  copyright">
+							<p>© 2021 Overhaul Media</p>
+						</div>
+						<div className="col-xs-12 col-sm-12 col-md-8 row nav-container">
+							<div class="col-md-6 nav">
+								<div className="mob-1">
+										<div itemProp="address" dangerouslySetInnerHTML={{ __html:address}}/>
+								</div>
+								<div className="mob-1">
+										<p><a itemProp="telephone" href={'tel:' + phone} rel="noreferrer" target="_blank" hrefLang="en">{phone}</a></p>
+										<p><a itemProp="email" href={'mailto:' + email} rel="noreferrer" target="_blank" hrefLang="en">{ email }</a></p>
+								</div>
+							</div>
+							<div class="col-md-6 nav">
+								<div className="mob-2">
+										<Link to="/ecommerce" rel="alternate" hrefLang="en">eCommerce</Link>
+										<Link to="/bar-restaurant-branding-edmonton" rel="alternate" hrefLang="en">Bar & Restaurant Branding</Link>
+								</div>
+								<div className="mob-2">
+										<Link to="/careers" rel="alternate" hrefLang="en">Careers</Link>
+										<Link to="/about#Services" rel="alternate" hrefLang="en">Services</Link>
+								</div>
+								<div className="mob-2">
+									<a href="https://www.instagram.com/overhaulmedia/?hl=en" target="_blank" rel="noreferrer" hrefLang="en">Instagram</a>
+									<a href="https://ca.linkedin.com/company/overhaul-media" target="_blank" rel="noreferrer" hrefLang="en">LinkedIn</a>
+								</div>
+							</div>
+						</div>
+					</div>
+			</footer>
+		)
+	}
 }
 
 export default Footer
