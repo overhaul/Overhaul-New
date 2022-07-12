a2dc1a62f6cd0765972f921709ca101e8a4bc72a
Roman <roman@overhaulmedia.com>
2022-07-12 16:52:53 -0600

    refactoring pageHero to use hook for innerwidth heigh of viewport

--- src/components/PageHero.js
+++ src/components/PageHero.js
@@ -1,23 +1,46 @@
 import * as React from 'react'
 import Img from "gatsby-image"
+import useWindowDimensions from '../hooks/useWindowDimensions'
 
-class PageHero extends React.Component {
-    render() {
-        const {title, subTitle, gatsbyImageData} = this.props
-        return (
-            <div className="page-hero">
-                {gatsbyImageData && (gatsbyImageData.fluid || gatsbyImageData.fixed) ? <Img
-                    {...gatsbyImageData}
-                    className="page-hero_img"
-                    />
-                : <div className="gatsby-image-wrapper"><img {...gatsbyImageData} /></div> }
-                <div className="container page-hero_container">
-                    { title ? <h1>{ title }</h1> : ''}
-                    { subTitle ? <h2>{ subTitle }</h2> : ''}
-                </div>
+// class PageHero extends React.Component {
+//     render() {
+//         const {title, subTitle, gatsbyImageData} = this.props
+//         const { height } = useWindowDimensions();
+//         return (
+//             <div className="page-hero" style={{ height : height }}>
+//                 {gatsbyImageData && (gatsbyImageData.fluid || gatsbyImageData.fixed) ? <Img
+//                     {...gatsbyImageData}
+//                     className="page-hero_img"
+//                     />
+//                 : <div className="gatsby-image-wrapper"><img {...gatsbyImageData} /></div> }
+//                 <div className="container page-hero_container">
+//                     { title ? <h1>{ title }</h1> : ''}
+//                     { subTitle ? <h2>{ subTitle }</h2> : ''}
+//                 </div>
+//             </div>
+//           )
+//       }
+// }
+
+// export default PageHero
+
+const PageHero = ({ title, subTitle, gatsbyImageData }) => {
+    // const { title, subTitle, gatsbyImageData } = this.props
+    const { height } = useWindowDimensions();
+
+    return (
+        <div className="page-hero" style={{ height : height }}>
+            {gatsbyImageData && (gatsbyImageData.fluid || gatsbyImageData.fixed) ? <Img
+                {...gatsbyImageData}
+                className="page-hero_img"
+                />
+            : <div className="gatsby-image-wrapper"><img {...gatsbyImageData} /></div> }
+            <div className="container page-hero_container">
+                { title ? <h1>{ title }</h1> : ''}
+                { subTitle ? <h2>{ subTitle }</h2> : ''}
             </div>
-          )
-      }
+        </div>
+    );
 }
 
 export default PageHero
