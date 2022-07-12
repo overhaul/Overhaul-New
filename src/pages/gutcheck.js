4a92a84ff24439aba07842c5b6596790ab9a9cb7
Roman <roman@overhaulmedia.com>
2022-07-12 14:51:03 -0600

    javascript solution to height

--- src/pages/gutcheck.js
+++ src/pages/gutcheck.js
@@ -2,11 +2,13 @@ import React, { useState, useRef } from 'react'
 import { Helmet } from "react-helmet"
 import StepForm from '../components/StepForm'
 import CursorWrapper from '../components/CursorWrapper'
+import WindowDimensions from '../components/WindowDimensions'
 
 const Form = () => {
 		const formRef = useRef(null)
 		const scriptUrl = "https://script.google.com/a/macros/overhaulmedia.com/s/AKfycbwE4Ed4-OEhVdeY_Kvfp2O-6GMlGc9-0Jwf9tXGJB8oDUFtxC1aRoGxgUj_HsVbX_lc/exec"
 		const [finished, setFinished] = useState(false)
+		const { height } = WindowDimensions();
 
 		const handleSubmit = (e) =>{
 				e.preventDefault()
@@ -20,6 +22,7 @@ const Form = () => {
 				})
 				.catch(err => console.log(err))
 		}
+		console.log(height)
 
 	return (
 		<div>
@@ -27,7 +30,7 @@ const Form = () => {
 				<meta name="robots" content="noindex, nofollow" />
 			</Helmet>
 			<CursorWrapper>
-				<div className="layout dark">
+				<div className="layout dark" style={{ height : height }}>
 					{ (!finished) ?
 						<form className="gutcheck" ref={formRef} onSubmit={handleSubmit} name="google-sheet" >
 							<StepForm />
