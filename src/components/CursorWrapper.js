01b71d84e1f19c6f43477a2e8d59a45d9cf579a0
Roman <roman@overhaulmedia.com>
2022-07-13 14:58:07 -0600

    can'y use navigator

--- src/components/CursorWrapper.js
+++ src/components/CursorWrapper.js
@@ -6,9 +6,11 @@ function CursorWrapper ({ children }) {
   const { slug: cursorSlug, text: cursorText } = useCursorType()
   const { x, y } = useMousePosition()
 
-  const userAgent = navigator.userAgent.toLowerCase();
-  const isMobile = /iPhone|Android/i.test(navigator.userAgent);
-  const isTablet = /(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/.test(userAgent);
+
+    const userAgent = typeof window !== 'undefined' && navigator.userAgent.toLowerCase();
+    const isMobile = /iPhone|Android/i.test(typeof window !== 'undefined' && navigator.userAgent);
+    const isTablet = /(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/.test(userAgent);
+
 
   if( isTablet || isMobile ) return null
   
