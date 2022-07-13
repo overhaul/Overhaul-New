e35324ddd86e1becda361d5cefd1318b885fb75d
Roman <roman@overhaulmedia.com>
2022-07-13 14:39:29 -0600

    getting rid of the cursor on tablets and mobile devices

--- src/components/CursorWrapper.js
+++ src/components/CursorWrapper.js
@@ -6,6 +6,12 @@ function CursorWrapper ({ children }) {
   const { slug: cursorSlug, text: cursorText } = useCursorType()
   const { x, y } = useMousePosition()
 
+  const userAgent = navigator.userAgent.toLowerCase();
+  const isMobile = /iPhone|Android/i.test(navigator.userAgent);
+  const isTablet = /(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/.test(userAgent);
+
+  if( isTablet || isMobile ) return null
+  
   return (
     <>
       <span
