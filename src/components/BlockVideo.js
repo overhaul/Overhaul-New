09ee361e863150f172cedc303cdb6c6298e31719
roman-overhaul <roman@overhaulmedia.com>
2022-11-22 14:51:25 -0800

    adding mobile styles

--- src/components/BlockVideo.js
+++ src/components/BlockVideo.js
@@ -18,7 +18,7 @@ class Vimeo extends React.Component {
         allowed.push('fullscreen');
       }
       return (
-        <div className="video-container wp">
+        <div className="video-container">
           <div class="player">
             <iframe
               className="play-vimeo"
