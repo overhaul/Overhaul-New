5625d64dff27f2afef7cb2d9a525be071db7678f
artemoverhaul <artem@overhaulmedia.com>
2024-08-29 10:26:05 -0600

    update config

--- gatsby-node.js
+++ gatsby-node.js
@@ -49,3 +49,24 @@ exports.createPages = async function ({graphql, actions}) {
     })
   )
 }
+
+// Copy favicon files to public folder after build
+exports.onPostBuild = () => {
+  const filesToCopy = [
+    { src: 'src/images/favicons/favicon.ico', dest: 'public/favicon.ico' },
+    { src: 'src/images/favicons/favicon.png', dest: 'public/favicon.png' },
+    { src: 'src/images/favicons/apple-touch-icon.png', dest: 'public/apple-touch-icon.png' },
+    { src: 'src/images/favicons/favicon-32x32.png', dest: 'public/favicon-32x32.png' },
+    { src: 'src/images/favicons/favicon-16x16.png', dest: 'public/favicon-16x16.png' },
+    { src: 'src/images/favicons/safari-pinned-tab.svg', dest: 'public/safari-pinned-tab.svg' },
+    { src: 'src/images/favicons/browserconfig.xml', dest: 'public/browserconfig.xml' },
+    { src: 'src/images/favicons/site.webmanifest', dest: 'public/site.webmanifest' },
+    { src: 'src/images/favicons/android-chrome-192x192.png', dest: 'public/android-chrome-192x192.png' },
+    { src: 'src/images/favicons/android-chrome-512x512.png', dest: 'public/android-chrome-512x512.png' },
+    { src: 'src/images/favicons/mstile-150x150.png', dest: 'public/mstile-150x150.png' },
+  ];
+
+  filesToCopy.forEach(file => {
+    fs.copyFileSync(path.resolve(file.src), path.resolve(file.dest));
+  });
+};
