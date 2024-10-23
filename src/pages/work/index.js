45ccf68730b118ac5c9d2e021c684a8c3543927a
artemoverhaul <artem@overhaulmedia.com>
2024-10-23 16:24:18 -0600

    x button fix

--- src/pages/work/index.js
+++ src/pages/work/index.js
@@ -108,7 +108,7 @@ class WorkPage extends Component {
                 ) : (
                   <>
                   {activeCategory && <li className="work-filter_item active-category">
-                    <button onClick={this.clearCategoryFilter}>X</button>
+                    <button onClick={this.clearCategoryFilter}>X&nbsp;</button>
                     <span>{activeCategory.title}</span>
                     {',\u00A0'}
                   </li>}
